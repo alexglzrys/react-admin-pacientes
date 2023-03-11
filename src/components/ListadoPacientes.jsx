4c06e9cb10d6c1e09855e55e0ccf2676f6c77ef5
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-11 15:16:01 -0600

    componente ListadoPacientes - estructura y diseño

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,24 @@
+import { Paciente } from "./Paciente";
+
 export const ListadoPacientes = () => {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
-      <h3 className="font-black text-center text-2xl mb-3">ListadoPacientes</h3>
+    <div className="md:w-1/2 lg:w-3/5 md:ml-5">
+      <h3 className="font-black text-center text-2xl mb-1">
+        Listado Pacientes
+      </h3>
+      <p className="text-center text-lg mb-5">
+        Administra tus{" "}
+        <span className="text-indigo-700 font-bold">Pacientes y Citas</span>
+      </p>
+      <div className="md:h-screen md:overflow-y-scroll mb-10">
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
+      </div>
     </div>
   );
 };
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,32 @@
+export const Paciente = () => {
+  return (
+    <div className="bg-white rounded-md py-10 px-5 shadow-sm mb-4">
+      <p className="font-bold uppercase mb-3 text-gray-700">
+        Mascota: <span className="font-normal normal-case">Mongo</span>
+      </p>
+      <p className="font-bold uppercase mb-3 text-gray-700">
+        Propietario:{" "}
+        <span className="font-normal normal-case">
+          Alejandro González Reyes
+        </span>
+      </p>
+      <p className="font-bold uppercase mb-3 text-gray-700">
+        Email:{" "}
+        <span className="font-normal normal-case">alejandro@correo.com</span>
+      </p>
+      <p className="font-bold uppercase mb-3 text-gray-700">
+        Fecha Registro:{" "}
+        <span className="font-normal normal-case">23 de Marzo de 2027</span>
+      </p>
+      <p className="font-bold uppercase mb-3 text-gray-700">
+        Síntomas:{" "}
+        <span className="font-normal normal-case">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+          distinctio et est obcaecati voluptatem neque provident asperiores
+          nobis? Quasi corporis suscipit inventore nihil consequatur quia
+          quibusdam cum harum dolores modi?
+        </span>
+      </p>
+    </div>
+  );
+};

ca07c4b45f17ba109f498459addefa3cfcd26c9b
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-11 11:28:49 -0600

    estilos componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,7 @@
 export const Header = () => {
-  return <div className="text-3xl">Header</div>;
+  return (
+    <h1 className="my-10 text-center font-bold text-4xl md:w-5/12 mx-auto">
+      Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span>
+    </h1>
+  );
 };
